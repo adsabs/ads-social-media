e9059cb8c0b6b614352ded2dee1f6a747d7c3b9a
Edwin Henneken <ehenneken@cfa.harvard.edu>
2013-05-29 11:12:35 -0400

    media_utils: streamlined function calls and skeleton for getting data from Mongo

--- ads-social-media/media_utils.py
+++ ads-social-media/media_utils.py
@@ -58,51 +58,48 @@ def get_keywords(bibcode):
            filter(lambda a: 'keyword' in a ,rsp['response']['docs'])))
     return uniq(filter(lambda a: a in IDENTIFIERS, keywords))
 
-def get_title_string(bibcode):
-    try:
-        data = get_article_data(bibcode)
-        return "%s: %s" % (data['first_author'], data['title'])
-    except:
-        return ''
+def get_publication_data(bibcode):
+#    get the article of the day for today from MongoDB
 
-def post_to_Facebook(bibcode, title):
+
+def post_to_Facebook(art_data):
     request_path = str(config.FACEBOOK_ID)+'/feed'
     post_data = {}
     post_data['access_token'] = config.FACEBOOK_ACCESS_TOKEN
     post_data['picture'] = ''
     post_data['caption'] = 'labs.adsabs.harvard.edu'
-    post_data['link'] = config.BASE_URL % bibcode.strip()
-    post_data['message'] = 'Article of the Day: %s' % title
+    post_data['link'] = config.BASE_URL % art_data['bibcode'].strip()
+    post_data['message'] = 'Article of the Day: %s' % art_data['title']
     post_data = urllib.urlencode(post_data)
     host = 'https://graph.facebook.com/%s' % request_path
     req = urllib2.Request(host,post_data)
     response = urllib2.urlopen(req)
     result = response.read()
 
-def post_to_Twitter(bibcode, header):
+def post_to_Twitter(art_data):
     try:
-        short_bibcode = get_short_bibcode(bibcode)
+        short_bibcode = art_data['short_bibcode']
     except:
         short_bibcode = bibcode
     url  = SHORT_URL % short_bibcode.strip()
     trailer = " %s %s" % (url,config.TWITTER_TAG)
     trailer_length = len(trailer)
     header_length = 140 - trailer_length - 5
-    post = "%s[...]%s" % (header[:header_length],trailer)
+    post = "%s[...]%s" % (art_data['twitter_header'][:header_length],trailer)
     auth = tweepy.OAuthHandler(config.TWITTER_CONSUMER_KEY, config.TWITTER_CONSUMER_SECRET)
     auth.set_access_token(config.TWITTER_ACCESS_KEY, config.TWITTER_ACCESS_SECRET)
     api = tweepy.API(auth)
     api.update_status(post)
 
-def post_to_Delicious(bibcode,title):
+def post_to_Delicious(art_data):
     a = DeliciousAPI(config.DELICIOUS_USER,config.DELICIOUS_PWD)
-    arturl = config.BASE_URL % bibcode.strip()
-    keywords = get_keywords(bibcode)
+    arturl = config.BASE_URL % art_data['bibcode'].strip()
+    keywords = get_keywords(art_data['bibcode'])
     entry_tags = ",".join(keywords)
-    a.posts_add(arturl, title, extended=config.EXTENDED_DESCRIPTION, tags=entry_tags)
+    a.posts_add(arturl, art_data['title'], extended=config.EXTENDED_DESCRIPTION, tags=entry_tags)
 
-def post_to_private_library(bibcode):
-    data = config.ADS_DATA % (bibcode.replace('&','%26'),this_year)
+def post_to_private_library(art_data):
+    data = config.ADS_DATA % (art_data['bibcode'].replace('&','%26'),this_year)
     headers = {'Cookie': config.ADS_COOKIE}
     req = urllib2.Request(config.ADS_URL,data,headers)
     f = urllib2.urlopen(req)
@@ -114,3 +111,8 @@ def post_article(bibcode,**args):
         targets = args['targets'].split(',')
     except:
         targets = ['Facebook','Twitter','Delicious','private_library']
+    data = get_publication_data(bibcode)
+    for target in targets:
+        func = globals()["post_to_%s"%target]
+        res = func(data)
+#    res = save_article_of_the_day(bibcode)
