b128e463fa96e8ba359213149809b1650800d81c
Edwin Henneken <ehenneken@cfa.harvard.edu>
2013-05-29 09:20:54 -0400

    __init__: create list with normalized keywords

--- ads-social-media/__init__.py
+++ ads-social-media/__init__.py
@@ -2,3 +2,11 @@
 from media_utils import get_articles_of_the_day
 # functions to post the Article of the Day
 from media_utils import post_article
+
+_basedir = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
+
+IDENTIFIER_FILE = _basedir + '/data/ASTkeywords.set'
+try:
+    IDENTIFIERS = open(IDENTIFIER_FILE).read().strip().split('\n')
+except:
+    IDENTIFIERS = []
